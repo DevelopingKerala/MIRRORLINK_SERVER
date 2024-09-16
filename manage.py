7382387cb02868a80aa0686e35fe5b92d9d0786f
DevelopingKerala <developingkerala@gmail.com>
2024-09-16 22:27:07 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
+warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
 
 
 def main():
